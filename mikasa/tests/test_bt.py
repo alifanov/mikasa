acea2e1be74e1feedd87f0d01d7febbc817dd543
Alexander Lifanov <lifanov.a.v@gmail.com>
2017-07-26 12:21:22 +0300

    add tests for BT class

--- mikasa/tests/test_bt.py
+++ mikasa/tests/test_bt.py
@@ -3,7 +3,7 @@ import pandas as pd
 from unittest import TestCase
 from .dataset import TEST_DATASET
 from ..data import DataSeries
-from ..bt import Trade
+from ..bt import Trade, BT
 
 
 class TradeTestCase(TestCase):
@@ -20,3 +20,51 @@ class TradeTestCase(TestCase):
         self.assertEqual(t.close_datetime, '2017-01-02')
         self.assertEqual(t.close_price, 200.0)
         self.assertEqual(t.get_profit(), 100.0)
+
+
+class BTTestCase(TestCase):
+    def test_bt(self):
+        df = pd.DataFrame(TEST_DATASET).rename(columns={
+            'Datetime': 'datetime',
+            'Open': 'open',
+            'High': 'high',
+            'Low': 'low',
+            'Close': 'close',
+        })
+        ds = DataSeries(df)
+        bt = BT(ds, balance=1.0)
+        self.assertEqual(bt.balance, 1.0)
+
+        bt.buy(ds[0].close, 1.0)
+        self.assertIsNotNone(bt.position)
+
+        bt.go()
+        bt.sell(ds[0].close)
+        self.assertIsNone(bt.position)
+        self.assertEqual(bt.get_profit(), 1.0)
+        self.assertEqual(bt.get_roi(), 1.0)
+
+    def test_bt_strat(self):
+        class SimpleBT(BT):
+            def process_bar(self):
+                if self.ds[0].close == 1.5:
+                    if not self.position:
+                        self.buy(self.ds[0].close, 1.0)
+                if self.ds[0].close == 2.5:
+                    if self.position:
+                        self.sell(self.ds[0].close)
+
+        df = pd.DataFrame(TEST_DATASET).rename(columns={
+            'Datetime': 'datetime',
+            'Open': 'open',
+            'High': 'high',
+            'Low': 'low',
+            'Close': 'close',
+        })
+        ds = DataSeries(df)
+        bt = SimpleBT(ds, balance=1.0)
+        bt.run()
+
+        self.assertIsNone(bt.position)
+        self.assertEqual(bt.get_profit(), 1.0)
+        self.assertEqual(bt.get_roi(), 1.0)
