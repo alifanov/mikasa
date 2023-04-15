668c525f4e74385ebcd7f58c5251fde621e794df
Aleksandr Lifanov <lifanov@welltory.app>
2023-04-15 18:59:25 +0200

    bump: version 0.0.2 → 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.1.0 (2023-04-15)
+
+### Feat
+
+- update executing orders
+
 ## 0.0.2 (2023-04-11)
 
 ### Fix
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="mikasa",
     packages=["mikasa"],
-    version="0.0.2",
+    version="0.1.0",
     description="Simple backtesting tool",
     author="Alexander Lifanov",
     author_email="lifanov.a.v@gmail.com",
