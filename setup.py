5fe4e08ba497a9323177e85ee97d555f2b482a3e
Aleksandr Lifanov <lifanov@welltory.app>
2023-04-15 19:57:05 +0200

    bump: version 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="mikasa",
     packages=["mikasa"],
-    version="0.1.1",
+    version="0.2.0",
     description="Simple backtesting tool",
     author="Alexander Lifanov",
     author_email="lifanov.a.v@gmail.com",
